c53b21f919b2d3359b1d290def2979e9573a8d99
Daniel-theDeveloper <danielalfarogamez@gmail.com>
2024-07-10 20:09:37 -0600

    probando

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,17 @@
-// module.exports = function (api) {  // Para compilar en web, es necesario activar esta linea
+module.exports = function (api) {  // Para compilar en web, es necesario activar esta linea
 
-export default function (api) {   // Para compilar en web, es necesario desactivar esta linea
+// export default function (api) {   // Para compilar en web, es necesario desactivar esta linea
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      [
-        "module-resolver",
-        {
-          extensions: [".tsx", ".ts", ".js", ".json"],
-        },
-      ],
-      "react-native-reanimated/plugin",
-    ],
+    // plugins: [
+    //   [
+    //     "module-resolver",
+    //     {
+    //       extensions: [".tsx", ".ts", ".js", ".json"],
+    //     },
+    //   ],
+    //   "react-native-reanimated/plugin",
+    // ],
   };
 };
